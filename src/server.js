001912156844e4c87fc3bd42e6547da3ac2a0664
김다빈 <135933231+slqkealr@users.noreply.github.com>
2023-10-11 21:25:41 +0900

    Responses

--- src/server.js
+++ src/server.js
@@ -4,6 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req,res) => {
+    return res.send("<h1>I Still love you.</h1>");
+}
+const handleLogin = (req,res) => {
+    return res.send({message : "Login here"});
+}
+
+app.get("/",handleHome);
+app.get("/login",handleLogin);
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 😃`);
 
 app.listen(PORT,handleListening);
