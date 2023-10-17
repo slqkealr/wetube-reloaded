607909a588d8969190288c0df1bc06b0e6354124
김다빈 <135933231+slqkealr@users.noreply.github.com>
2023-10-17 21:31:46 +0900

    Morgan Middleware

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req,res,next) => {
+const middleWare = (req,res,next) => {
     console.log(`${req.method} ${req.url}`);
     next();
 }
@@ -13,8 +15,8 @@ const handleHome = (req,res) => {
     return res.send("I love middlewares!");
 }
 
-
-app.get("/",logger,handleHome);
+app.use(logger);
+app.get("/",middleWare,handleHome);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 😃`);
 
