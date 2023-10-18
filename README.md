564d280664e623d485967fa71d071af3c921b25c
김다빈 <135933231+slqkealr@users.noreply.github.com>
2023-10-18 22:11:53 +0900

    Exports

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# lalalalala!
+# Wetube Reloaded
+
+/ -> home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a Video
+/videos/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,24 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const middleWare = (req,res,next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-
-const handleHome = (req,res) => {
-    return res.send("I love middlewares!");
-}
-
 app.use(logger);
-app.get("/",middleWare,handleHome);
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 😃`);
 
-app.listen(PORT,handleListening);
+app.use("/",globalRouter);
+app.use("/users",userRouter);
+app.use("/videos",videoRouter);
 
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
