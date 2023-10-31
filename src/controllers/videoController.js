cfa4d75289ce543fae3a6771c6956a0057b11601
김다빈 <135933231+slqkealr@users.noreply.github.com>
2023-10-31 21:54:39 +0900

    MVP Styles

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Comes from your controller" });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
