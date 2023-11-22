6cb563b6bca9e761a24e4493c3cee1c71589ccd2
김다빈 <135933231+slqkealr@users.noreply.github.com>
2023-11-22 23:25:53 +0900

    Creating a Video part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,18 +31,17 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
+  await Video.create({
     title,
     description,
     createdAt: Date.now(),
     hashtags: hashtags.split(",").map((word) => `#${word}`),
     meta: {
-      view: 0,
+      views: 0,
       rating: 0,
     },
   });
-  console.log(video);
   return res.redirect("/");
 };
