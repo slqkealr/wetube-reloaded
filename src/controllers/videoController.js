fdf9a01f020a830d8a48353280a5abb0e166ed21
김다빈 <moodiseo@gmail.com>
2024-01-23 22:56:01 +0900

    #6.25 Delete Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -54,3 +54,9 @@ export const postUpload = async (req, res) => {
     });
   }
 };
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
