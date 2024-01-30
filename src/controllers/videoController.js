32c45390855bf8f0a8fa7c3e5df97449214c4d3c
김다빈 <moodiseo@gmail.com>
2024-01-30 22:55:43 +0900

    #6.27 Search part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -65,8 +65,11 @@ export const search = async (req, res) => {
   let videos = [];
   if (keyword) {
     videos = await Video.find({
-      title: keyword,
+      title: {
+        $regex: new RegExp(`${keyword}$`, "i"),
+      },
     });
+    console.log(videos);
   }
   return res.render("search", { pageTitle: "Search", videos });
 };
